fb1a4f44f9473adba533fc1ca61bf05ed3374786
Ben Purinton <ben.purinton@gmail.com>
2025-07-31 12:33:30 -0700

    reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,52 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_24_165316) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_13_202311) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "actors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.date "dob"
-    t.text "bio"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "credits", force: :cascade do |t|
-    t.string "role"
-    t.integer "actor_id"
-    t.integer "movie_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "directors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.date "dob"
-    t.text "bio"
-    t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "movies_count"
-  end
-
-  create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.integer "year"
-    t.integer "duration"
-    t.text "description"
-    t.string "image"
-    t.integer "director_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.date "released_on"
-    t.integer "oscar_cohort"
-    t.string "result", default: "nominated"
-  end
 
   create_table "solid_cable_messages", force: :cascade do |t|
     t.binary "channel", null: false
