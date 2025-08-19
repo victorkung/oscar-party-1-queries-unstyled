65905cdf753078c67323ab53a7e39506fcd053ef
Victor Kung <victor.y.kung@gmail.com>
2025-08-19 22:25:30 +0000

    Moved variable into controller and make tweaks to index to match target.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
 
   def index
+    @list_of_movies = Movie.all.order({ :year => :desc, :title => :asc })
     render({ :template => "movie_templates/index" })
   end
 
